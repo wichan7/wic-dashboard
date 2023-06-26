b5a506fbb8b83f0714749e56942d9b2cc4e78ace
wichan7 <wichan7@naver.com>
2023-06-26 17:03:15 +0900

    수정: combine morgan winston

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
+// logger
+let morgan = require('morgan');
+let logger = require('./utils/logger');
+let stream = { write: (message) => { logger.http(message) } };
 
 let app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(morgan('combined', { stream }));
+
 /* public 폴더 지정 */
 app.use(express.static(path.join(__dirname, 'public')));
 
